b07f8d6ece567ad82393e1426f1253b4deb290b4
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-07-23 16:39:20 -0400

    feat: start about me, wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Hero from "./components/Hero";
-import Projects from "./components/Projects";
+import React from 'react';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import AboutMe from './components/AboutMe';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Hero />
-      <Projects/>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between">
+			<Hero />
+			<Projects />
+			<AboutMe />
+		</main>
+	);
 }
