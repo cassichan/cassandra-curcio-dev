eb7d53d825abbb2ed214ec1c8c7871984d9bf0c7
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-07-23 14:42:38 -0400

    add social logo with links and style hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,13 +14,13 @@ export default function Hero() {
 					/>
 				</div>
 				<div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-300">
+					<h1 className="title-font sm:text-4xl text-3xl mb-8 font-medium text-gray-300">
 						I&apos;m Cassandra.
 						<br className="hidden lg:inline-block" />
 						Nice to meet you! ✨
 					</h1>
-					<p className="mb-8 leading-relaxed font-medium text-gray-300">Software Engineer with a background in Healthcare and Project Management 👩‍💻 </p>
-					<p className="mb-8 leading-relaxed font-medium text-gray-300">I am looking for my next career opportunity so please reach out to see how I can help your team!</p>
+					<p className="mb-2 leading-relaxed font-medium text-gray-300">Software Engineer with a background in Healthcare and Project Management 👩‍💻 </p>
+					<p className="mb-8 leading-relaxed font-medium text-gray-300">I am looking for my next career opportunity. Please reach out to see how I can help your team!</p>
 					<div className="flex justify-center">
 						<a
 							href="https://docs.google.com/document/d/15GM5uc_WCekFqrclNi2-AW8xl1Y2qdjvGTno2DbRWsw/edit?usp=sharing"
@@ -30,6 +30,48 @@ export default function Hero() {
 							<button className="inline-flex text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-teal-600 rounded text-lg">Resume</button>
 						</a>
 						<button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">About me</button>
+					</div>
+					<br className="hidden lg:inline-block" />
+					<div className="flex mt-10">
+						<a
+							href="https://github.com/cassichan"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image
+								className="m-3"
+								alt="email"
+								src="/../public/socials/github-mark-white.png"
+								height={30}
+								width={55}
+							/>
+						</a>
+						<a
+							href="mailto:cassandra.curcio@gmail.com"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image
+								className="m-3"
+								alt="email"
+								src="/../public/socials/email.png"
+								height={30}
+								width={55}
+							/>
+						</a>
+						<a
+							href="https://www.linkedin.com/in/cassandra-l-curcio/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image
+								className="mt-5 ml-3 mr-3"
+								alt="email"
+								src="/../public/socials/ln-white-1.png"
+								height={30}
+								width={55}
+							/>
+						</a>
 					</div>
 				</div>
 			</div>
